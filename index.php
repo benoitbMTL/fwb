55a21c145a22d379a2f4a48cffb3f86c31c22d83
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2022-04-06 13:49:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -6,15 +5,20 @@
   </head>
   <body>
     <?php print "<h1>You are on ". $_SERVER['SERVER_NAME'] . "</h1>" ?>
-    
+
+    <p>--------------------------------------------------</p>
     <p><b>Hostname: </b><?php echo gethostname(); ?></p>
     <p><b>Address: </b><?php echo $_SERVER['SERVER_ADDR']; ?></p>
-<ul>
-<li><a href="/dvwa">dvwa</a></li>
-<li><a href="/badsite.html">badsite</a></li>
-<li><a href="/wso.php">webshell</a></li>
-<li><a href="/dvwa/hackable/uploads/">uploads</a></li>
-</ul>
+    <p><b>PHPSESSID: </b><?php echo $_COOKIE["PHPSESSID"]; ?></p>
+    <p><b>Security: </b><?php echo $_COOKIE["security"]; ?></p>
+    <p>--------------------------------------------------</p>
+
+    <ul>
+    <li><a href="/dvwa">dvwa</a></li>
+    <li><a href="/badsite.html">badsite</a></li>
+    <li><a href="/wso.php">webshell</a></li>
+    <li><a href="/dvwa/hackable/uploads/">uploads</a></li>
+    </ul>
 
     <p>--------------------------------------------------</p>
     <?php print "REMOTE_ADDR : <b style=\"color:red;\">" . $_SERVER['REMOTE_ADDR'] . "</b>" ?>
@@ -25,12 +29,14 @@
     <p></p>
     <?php print "REMOTE_USER : <b style=\"color:red;\">" . $_SERVER['REMOTE_USER'] . "</b>" ?>
     <p>--------------------------------------------------</p>
+
     <?php print "SERVER_ADDR : <b style=\"color:red;\">" . $_SERVER['SERVER_ADDR'] . "</b>" ?>
     <p></p>
     <?php print "SERVER_NAME : <b style=\"color:red;\">" . $_SERVER['SERVER_NAME'] . "</b>" ?>
     <p></p>
     <?php print "SERVER_SOFTWARE : <b style=\"color:red;\">" . $_SERVER['SERVER_SOFTWARE'] . "</b>" ?>
     <p>--------------------------------------------------</p>
+
     <?php print "HTTP_X_FORWARDED_FOR : <b style=\"color:red;\">" . $_SERVER['HTTP_X_FORWARDED_FOR'] . "</b>" ?>
     <p></p>
     <?php print "HTTP_CLIENT_IP : <b style=\"color:red;\">" . $_SERVER['HTTP_CLIENT_IP'] . "</b>" ?>
