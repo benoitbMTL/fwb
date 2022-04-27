d7cbbfe87f5ac850e204dd09171696140783dd9a
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2022-04-27 10:47:48 -0400

    Create api-test.sh

---
+++ api-test.sh
@@ -0,0 +1,252 @@
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1,
+  "username": "a",
+  "firstName": "a",
+  "lastName": "a",
+  "email": "a@a.ca",
+  "password": "a",
+  "phone": 1,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 11,
+  "username": "aa",
+  "firstName": "aa",
+  "lastName": "aa",
+  "email": "aa@aa.ca",
+  "password": "aa",
+  "phone": 11,
+  "userStatus": 0
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 111,
+  "username": "aaa",
+  "firstName": "aaa",
+  "lastName": "aaa",
+  "email": "aaa@aaa.ca",
+  "password": "aaa",
+  "phone": 111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1111,
+  "username": "aaaa",
+  "firstName": "aaaa",
+  "lastName": "aaaa",
+  "email": "aaaa@aaaa.ca",
+  "password": "aaaa",
+  "phone": 1111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 11111,
+  "username": "aaaaa",
+  "firstName": "aaaaa",
+  "lastName": "aaaaa",
+  "email": "aaaaa@aaaaa.ca",
+  "password": "aaaaa",
+  "phone": 11111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 111111,
+  "username": "aaaaaa",
+  "firstName": "aaaaaa",
+  "lastName": "aaaaaa",
+  "email": "aaaaaa@aaaaaa.ca",
+  "password": "aaaaaa",
+  "phone": 111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1111111,
+  "username": "aaaaaaa",
+  "firstName": "aaaaaaa",
+  "lastName": "aaaaaaa",
+  "email": "aaaaaaa@aaaaaaa.ca",
+  "password": "aaaaaaa",
+  "phone": 1111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 11111111,
+  "username": "aaaaaaaa",
+  "firstName": "aaaaaaaa",
+  "lastName": "aaaaaaaa",
+  "email": "aaaaaaaa@aaaaaaaa.ca",
+  "password": "aaaaaaaa",
+  "phone": 11111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 111111111,
+  "username": "aaaaaaaaa",
+  "firstName": "aaaaaaaaa",
+  "lastName": "aaaaaaaaa",
+  "email": "aaaaaaaaa@aaaaaaaaa.ca",
+  "password": "aaaaaaaaa",
+  "phone": 111111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1111111111,
+  "username": "aaaaaaaaaa",
+  "firstName": "aaaaaaaaaa",
+  "lastName": "aaaaaaaaaa",
+  "email": "aaaaaaaaaa@aaaaaaaaaa.ca",
+  "password": "aaaaaaaaaa",
+  "phone": 1111111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 11111111111,
+  "username": "aaaaaaaaaaa",
+  "firstName": "aaaaaaaaaaa",
+  "lastName": "aaaaaaaaaaa",
+  "email": "aaaaaaaaaaa@aaaaaaaaaaa.ca",
+  "password": "aaaaaaaaaaa",
+  "phone": 11111111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 111111111111,
+  "username": "aaaaaaaaaaaa",
+  "firstName": "aaaaaaaaaaaa",
+  "lastName": "aaaaaaaaaaaa",
+  "email": "aaaaaaaaaaaa@aaaaaaaaaaaa.ca",
+  "password": "aaaaaaaaaaaa",
+  "phone": 111111111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1111111111111,
+  "username": "aaaaaaaaaaaaa",
+  "firstName": "aaaaaaaaaaaaa",
+  "lastName": "aaaaaaaaaaaaa",
+  "email": "aaaaaaaaaaaaa@aaaaaaaaaaaaa.ca",
+  "password": "aaaaaaaaaaaaa",
+  "phone": 1111111111111,
+  "userStatus": 1
+}'
+
+sleep 1
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 11111111111111,
+  "username": "aaaaaaaaaaaaaa",
+  "firstName": "aaaaaaaaaaaaaa",
+  "lastName": "aaaaaaaaaaaaaa",
+  "email": "aaaaaaaaaaaaaa@aaaaaaaaaaaaaa.ca",
+  "password": "aaaaaaaaaaaaaa",
+  "phone": 11111111111111,
+  "userStatus": 1
+}'
+
+
+
