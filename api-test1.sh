83b43a43feed42e6d5967891a7129fa2c7597e3d
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2022-04-27 12:09:43 -0400

    Update api-test1.sh

--- api-test1.sh
+++ api-test1.sh
@@ -1,4 +1,4 @@
-echo "creating id : 1 -- size 1"
+echo "creating id : 0 -- size 1"
 
 curl -X 'POST' \
   --silent --insecure \
@@ -6,7 +6,7 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "id": 1,
+  "id": 0,
   "username": "a",
   "firstName": "a",
   "lastName": "a",
@@ -18,7 +18,7 @@ curl -X 'POST' \
 
 sleep 1
 echo ""
-echo "creating id : 2 -- size 2"
+echo "creating id : 0 -- size 1"
 
 curl -X 'POST' \
   --silent --insecure \
@@ -26,13 +26,13 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "id": 2,
+  "id": 0,
   "username": "a",
   "firstName": "a",
   "lastName": "a",
   "email": "a@a.ca",
   "password": "a",
-  "phone": "5141324564",
+  "phone": "5146042906",
   "userStatus": 1
 }'
 
@@ -54,6 +54,26 @@ curl -X 'POST' \
   "password": "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
   "phone": "5146042906",
   "userStatus": 1
+}'
+
+sleep 1
+echo ""
+echo "creating id : 1 -- size 1"
+
+curl -X 'POST' \
+  --silent --insecure \
+  'https://petstore.swagger.io/v2/user' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 1,
+  "username": "a",
+  "firstName": "a",
+  "lastName": "a",
+  "email": "a@a.ca",
+  "password": "a",
+  "phone": "5144563579",
+  "userStatus": 0
 }'
 
 sleep 1
