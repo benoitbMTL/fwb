366958742e641f28bc500a16d779ffa56f59cdd8
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2022-04-27 12:13:41 -0400

    Update api-test2.sh

--- api-test2.sh
+++ api-test2.sh
@@ -1,4 +1,4 @@
-echo "creating id : 0 -- size 1"
+echo "creating id : 1 -- size 1"
 
 curl -X 'POST' \
   --silent --insecure \
@@ -6,27 +6,7 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "id": 0,
-  "username": "a",
-  "firstName": "a",
-  "lastName": "a",
-  "email": "a@a.ca",
-  "password": "a",
-  "phone": "5146042906",
-  "userStatus": 1
-}'
-
-sleep 1
-echo ""
-echo "creating id : 0 -- size 1"
-
-curl -X 'POST' \
-  --silent --insecure \
-  'https://petstore.swagger.io/v2/user' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "id": 0,
+  "id": 1,
   "username": "a",
   "firstName": "a",
   "lastName": "a",
@@ -52,8 +32,8 @@ curl -X 'POST' \
   "lastName": "a",
   "email": "a@a.ca",
   "password": "a",
-  "phone": "5144563579",
-  "userStatus": 0
+  "phone": "5146042906",
+  "userStatus": 1
 }'
 
 sleep 1
