55a4aaf1747b67536c695dc2241c8a023ed11cf5
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2022-06-08 23:46:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ echo "-------------------------------------------------------\r\n";
 echo "\r\n";
 
 echo "PHPSESSID:           " . $_COOKIE["PHPSESSID"] . "\r\n";
-echo "security:            " . $_COOKIE["security"] . "\r\n";
+echo "Security:            " . $_COOKIE["security"] . "\r\n";
+echo "Cookiesession1:      " . $_COOKIE["cookiesession1"] . "\r\n";
 
 echo "\r\n";
 echo "-------------------------------------------------------\r\n";
