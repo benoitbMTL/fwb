16be2cc7eb7de1f3688a5a460d7c5f7b35055b0e
benoitbMTL <benoit@buonassera.fr>
2023-05-05 01:06:08 -0400

    reset ml with $domain_name

--- fl-demo/ml-reset-domain.sh
+++ fl-demo/ml-reset-domain.sh
@@ -4,30 +4,36 @@
 ### CONFIGURE TOKEN ###
 TOKEN="eyJ1c2VybmFtZSI6InVzZXJhcGkiLCJwYXNzd29yZCI6ImZhY2VMT0NLeWFybjY3ISJ9Cg=="
 
-### CONFIGURE HOST - Primary FortiWeb IP ###
+### CONFIGURE HOST - Primary FortiWeb IP
 #HOST="192.168.4.2"
 HOST="10.163.7.21"
 
 ### CONFIGURE POLICY NAME
-POLICY="DVWA_POLICY"
+POLICY="main-policy"
 
 ### getdomaininfo and store result
-echo "Get Domain Info"
+echo "Getting Domain Info with API"
+echo ""
+
 result=$(curl --insecure --location -g --request GET "https://${HOST}/api/v2.0/machine_learning/policy.getdomaininfo?policy_name=${POLICY}" \
 --header "Authorization: ${TOKEN}" \
 --header 'accept: application/json' | jq)
 
-echo "-------------------------------------"
-#echo "Step 1 Result: ${result}"
-echo "-------------------------------------"
-
+echo ""
+echo "--------------------------------------------------------------"
+echo "$result"
+echo "--------------------------------------------------------------"
+echo ""
 
 # Get the db_id value
 db_id=$(echo "$result" | jq '.results[0].db_id')
+domain_name=$(echo "$result" | jq '.results[0].domain_name')
 
-echo "-------------------------------------"
-echo "Domain db_id to reset is: ${db_id}"
-echo "-------------------------------------"
+echo ""
+echo "--------------------------------------------------------------"
+echo "Domain ${domain_name} has db_id ${db_id}"
+echo "--------------------------------------------------------------"
+echo ""
 
 
 # Execute the command with the extracted db_id value
@@ -37,6 +43,7 @@ curl --insecure --location -g --request POST "https://${HOST}/api/v2.0/machine_l
 --header 'accept: application/json' \
 --data-raw '{"domain_id": "'${db_id}'", "policy_name": "'${POLICY}'"}'
 
-echo "-------------------------------------"
-echo "Machine Learning for domain is reset"
-echo "-------------------------------------"
+echo ""
+echo "--------------------------------------------------------------"
+echo "Machine Learning for domain ${domain_name} has been reset"
+echo "--------------------------------------------------------------"
