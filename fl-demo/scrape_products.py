5fbb02104ea414b641b31f5c95ddef42dd26f027
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-05-14 14:26:06 -0400

    true

--- fl-demo/scrape_products.py
+++ fl-demo/scrape_products.py
@@ -9,7 +9,7 @@ column_width = 80
 table_width = 250
 round = 0
 # adjust this to False if you do not want to output anything
-output = False
+output = True
 headers = {
     "Accept": "application/json",
 }
