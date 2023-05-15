887d9dbf7e8e6b15310fed89dcc7412e1bc60504
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-05-15 18:45:01 -0400

    curl -s

--- fl-demo/petstore-demo.sh
+++ fl-demo/petstore-demo.sh
@@ -21,21 +21,21 @@ options=(
 )
 
 curl_commands=(
-    "curl -v -X GET 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=available' -H 'accept: application/json' -H 'content-type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=pending' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=ABCDEFGHIJKL' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=A' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=;cmd.exe' -H 'Accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"ls;;cmd.exe\"}'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold&status=pending' -H 'accept: application/json' -H 'Content-Type: application/json'"
-    "curl -v -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold' -H 'accept: application/yaml' -H 'Content-Type: application/json'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"ls;;cmd.exe\"}'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}'"
-    "curl -v -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}'"
+    "curl -s -X GET 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=available' -H 'accept: application/json' -H 'content-type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=pending' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=ABCDEFGHIJKL' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=A' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=;cmd.exe' -H 'Accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"ls;;cmd.exe\"}'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold&status=pending' -H 'accept: application/json' -H 'Content-Type: application/json'"
+    "curl -s -X 'GET' 'http://petstore.corp.fabriclab.ca/v2/pet/findByStatus?status=sold' -H 'accept: application/yaml' -H 'Content-Type: application/json'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"ls;;cmd.exe\"}'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}'"
+    "curl -s -X 'POST' 'http://petstore.corp.fabriclab.ca/v2/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}'"
 )
 
 select_option() {
