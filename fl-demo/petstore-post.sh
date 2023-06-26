e382006543175041831b60e280bf26c0259d0625
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 19:38:37 -0400

    post get merged

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -2,8 +2,7 @@
 
 PETSTORE_URL='https://petstore.buonassera.fr/api/v3'
 #PETSTORE_URL='http://petstore.corp.fabriclab.ca/api/v3'
-
-ID=420
+USER_AGENT="ML-API-Demo-Tool"
 
 verbose=false
 
@@ -29,8 +28,9 @@ do
   RANDOM_PET=$(generate_random_value "Puma" "Fish" "Spider" "Tiger" "Lion" "Shark" "Snake" "Monkey" "Fox" "Ram" "Eagle" "Bee" "Cat" "Dog" "Ant" "Wasp" "Panter" "Gator" "Owl" "Wildcats")
   RANDOM_STATUS=$(generate_random_value "available" "pending" "sold")
   RANDOM_IP=$(shuf -i 0-255 -n 4 | paste -sd '.')
-
-  curl_common="-k -A ML-API-Demo-Tool -X 'POST' '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -H 'X-Forwarded-For: ${RANDOM_IP}' -d '{\"id\": $ID, \"category\": {\"id\": $ID, \"name\": \"${RANDOM_PET}\"}, \"name\": \"${RANDOM_NAME}\", \"photoUrls\": [\"http://surl.li/imgkr\"], \"tags\": [{\"id\": $ID, \"name\": \"${RANDOM_NAME}\"}], \"status\": \"${RANDOM_STATUS}\"}'"
+  RANDOM_ID=$(shuf -i 1-999 -n 1)
+  
+  curl_common="--insecure --user-agent '${USER_AGENT}' --request 'POST' '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/xml' -H 'X-Forwarded-For: ${RANDOM_IP}' -d '{\"id\": $RANDOM_ID, \"category\": {\"id\": $RANDOM_ID, \"name\": \"${RANDOM_PET}\"}, \"name\": \"${RANDOM_NAME}\", \"photoUrls\": [\"http://surl.li/imgkr\"], \"tags\": [{\"id\": $RANDOM_ID, \"name\": \"${RANDOM_NAME}\"}], \"status\": \"${RANDOM_STATUS}\"}'"
 
   if $verbose; then
     curl_cmd="curl ${curl_common}"
@@ -45,7 +45,21 @@ do
     echo -ne "Requests sent (POST): $((i))\r"
   fi
 
-  ID=$((ID+1))
+  curl_common="--insecure --user-agent '${USER_AGENT}' --request 'GET' '${PETSTORE_URL}/pet/findByStatus?status=${RANDOM_STATUS}' -H 'accept: application/xml' -H 'Content-Type: application/json' -H 'X-Forwarded-For: ${RANDOM_IP}'"
+
+  if $verbose; then
+    curl_cmd="curl ${curl_common}"
+    echo "${i}: ${curl_cmd}"
+    echo -ne "\033[32mResult:\033[0m "
+    eval "${curl_cmd}"
+    echo ""
+    echo ""
+  else
+    curl_cmd="curl -s -o /dev/null ${curl_common}"
+    eval "${curl_cmd}"
+    echo -ne "Requests sent (GET): $((i))\r"
+  fi
+
 done
 
 echo ""
