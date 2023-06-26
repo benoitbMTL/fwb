16007bb7c87de49bff4616dd746b9e19c94ef424
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 12:49:58 -0400

    aa

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -14,6 +14,7 @@ do
   curl_cmd="curl -k -A ML-API-Demo-Tool -X 'POST' '${PETSTORE_URL}/pet' -H 'accept: application/xml' -H 'Content-Type: application/json' -d '{\"id\": $ID, \"category\": {\"id\": $ID, \"name\": \"${PETS[$i]}\"}, \"name\": \"${NAMES[$i]}\", \"photoUrls\": [\"Willupdatelater\"], \"tags\": [{\"id\": $ID, \"name\": \"${NAMES[$i]}\"}], \"status\": \"${STATUS[$i]}\"}'"
 
   echo "${i}: ${curl_cmd}"
+  echo ""
   eval "${curl_cmd}"
   echo ""
   echo ""
