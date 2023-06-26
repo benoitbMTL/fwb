94e3c4c2a560a25652665c41b343c353016535ff
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 13:22:10 -0400

    verbose mode

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -9,19 +9,29 @@ NAMES=(FortiPuma FortiFish FortiSpider FortiTiger FortiLion FortiShark FortiSnak
 PETS=(Puma Fish Spider Tiger Lion Shark Snake Monkey Fox Ram Eagle Bee Cat Dog Ant Wasp Panter Gator Owl Wildcats)
 STATUS=(available pending sold available pending sold available pending sold available pending sold available pending sold available pending sold available pending)
 
+verbose=false
+
+if [ "$1" == "-v" ] || [ "$1" == "--verbose" ]; then
+  verbose=true
+fi
+
 echo "Sending POST API calls to ${PETSTORE_URL} to populate pets entries with FortiPets"
 echo ""
 
-for ((i=1; i<20; i++))
+for ((i=1; i<21; i++))
 do
   curl_cmd="curl -k -A ML-API-Demo-Tool -X 'POST' '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"id\": $ID, \"category\": {\"id\": $ID, \"name\": \"${PETS[$i]}\"}, \"name\": \"${NAMES[$i]}\", \"photoUrls\": [\"http://surl.li/imgkr\"], \"tags\": [{\"id\": $ID, \"name\": \"${NAMES[$i]}\"}], \"status\": \"${STATUS[$i]}\"}'"
 
-  echo "${i}: ${curl_cmd}"
-  echo -ne "\033[32mResult:\033[0m "
-  eval "${curl_cmd}"
-  echo ""
-  echo ""
-  
+  if $verbose; then
+    echo "${i}: ${curl_cmd}"
+    echo -ne "\033[32mResult:\033[0m "
+    eval "${curl_cmd}"
+    echo ""
+    echo ""
+  else
+    eval "${curl_cmd}"
+  fi
+
   ID=$((ID+1))
 
   echo -ne "Requests sent (POST): $((i))\r"
