9577e51d1922028b9be45490e8adf67752532700
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 21:04:13 -0400

    echo

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -4,7 +4,7 @@ PETSTORE_URL='https://petstore.buonassera.fr/api/v3'
 #PETSTORE_URL='http://petstore.corp.fabriclab.ca/api/v3'
 USER_AGENT="ML-API-Demo-Tool"
 
-REQUESTS=500
+REQUESTS=5
 
 verbose=false
 
@@ -60,6 +60,7 @@ do
     echo "${i}: ${curl_cmd}"
     echo -ne "\033[32mDELETE Result:\033[0m "
     eval "${curl_cmd}"
+    echo ""
     echo ""
     echo ""
   else
