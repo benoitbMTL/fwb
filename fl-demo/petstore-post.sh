d35147063a3a18a5d029d0484196ccae8daa95df
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 21:00:57 -0400

    500

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -4,7 +4,7 @@ PETSTORE_URL='https://petstore.buonassera.fr/api/v3'
 #PETSTORE_URL='http://petstore.corp.fabriclab.ca/api/v3'
 USER_AGENT="ML-API-Demo-Tool"
 
-REQUESTS=5
+REQUESTS=500
 
 verbose=false
 
