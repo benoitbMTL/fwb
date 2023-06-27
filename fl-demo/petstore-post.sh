50d149959d11c0485962609f9f6654677de5fbe5
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-26 20:39:57 -0400

    PUT

--- fl-demo/petstore-post.sh
+++ fl-demo/petstore-post.sh
@@ -4,7 +4,7 @@ PETSTORE_URL='https://petstore.buonassera.fr/api/v3'
 #PETSTORE_URL='http://petstore.corp.fabriclab.ca/api/v3'
 USER_AGENT="ML-API-Demo-Tool"
 
-REQUESTS=500
+REQUESTS=5
 
 verbose=false
 
@@ -20,7 +20,7 @@ generate_random_value() {
   echo "${array[random_index]}"
 }
 
-echo "Sending ${REQUESTS} POST & GET API calls to ${PETSTORE_URL} to add & get pets entries"
+echo "Sending ${REQUESTS} POST/GET/PUT API calls to ${PETSTORE_URL} to add & get pets entries"
 echo ""
 
 for ((i=1; i<=$REQUESTS; i++))
@@ -29,11 +29,13 @@ do
   RANDOM_NAME=$(generate_random_value "FortiPuma" "FortiFish" "FortiSpider" "FortiTiger" "FortiLion" "FortiShark" "FortiSnake" "FortiMonkey" "FortiFox" "FortiRam" "FortiEagle" "FortiBee" "FortiCat" "FortiDog" "FortiAnt" "FortiWasp" "FortiPanter" "FortiGator" "FortiOwl" "FortiWildcats")
   RANDOM_PET=$(generate_random_value "Puma" "Fish" "Spider" "Tiger" "Lion" "Shark" "Snake" "Monkey" "Fox" "Ram" "Eagle" "Bee" "Cat" "Dog" "Ant" "Wasp" "Panter" "Gator" "Owl" "Wildcats")
   RANDOM_STATUS=$(generate_random_value "available" "pending" "sold")
+  RANDOM_STATUS_NEW=$(generate_random_value "available" "pending" "sold")
   RANDOM_IP=$(shuf -i 0-255 -n 4 | paste -sd '.')
   RANDOM_ID=$(shuf -i 1-999 -n 1)
   
   curl_post="--insecure --user-agent '${USER_AGENT}' --request 'POST' '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -H 'X-Forwarded-For: ${RANDOM_IP}' -d '{\"id\": $RANDOM_ID, \"category\": {\"id\": $RANDOM_ID, \"name\": \"${RANDOM_PET}\"}, \"name\": \"${RANDOM_NAME}\", \"photoUrls\": [\"http://surl.li/imgkr\"], \"tags\": [{\"id\": $RANDOM_ID, \"name\": \"${RANDOM_NAME}\"}], \"status\": \"${RANDOM_STATUS}\"}'"
   curl_get="--insecure --silent --user-agent '${USER_AGENT}' --request 'GET' '${PETSTORE_URL}/pet/findByStatus?status=${RANDOM_STATUS}' -H 'accept: application/json' -H 'Content-Type: application/json' -H 'X-Forwarded-For: ${RANDOM_IP}' | jq length"
+  curl_put="--insecure --user-agent '${USER_AGENT}' --request 'PUT' '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json' -H 'X-Forwarded-For: ${RANDOM_IP}' -d '{\"id\": $RANDOM_ID, \"category\": {\"id\": $RANDOM_ID, \"name\": \"${RANDOM_PET}\"}, \"name\": \"${RANDOM_NAME}\", \"photoUrls\": [\"https://tinyurl.com/mtn5wrwu\"], \"tags\": [{\"id\": $RANDOM_ID, \"name\": \"${RANDOM_NAME}\"}], \"status\": \"${RANDOM_STATUS_NEW}\"}'"
 
   if $verbose; then
     curl_cmd="curl ${curl_post}"
@@ -48,15 +50,23 @@ do
     eval "${curl_cmd}"
     echo ""
     echo ""
+    curl_cmd="curl ${curl_put}"
+    echo "${i}: ${curl_cmd}"
+    echo -ne "\033[32mPOST Result:\033[0m "
+    eval "${curl_cmd}"
+    echo ""
+    echo ""
   else
     curl_cmd="curl -s -o /dev/null ${curl_post}"
     eval "${curl_cmd}"
     curl_cmd="curl -s -o /dev/null ${curl_get}"
     eval "${curl_cmd}"
-    echo -ne "Number of POST: $i | Number of GET: $i\r"
+    curl_cmd="curl -s -o /dev/null ${curl_put}"
+    eval "${curl_cmd}"
+    echo -ne "Number of POST: $i | Number of GET: $i | Number of PUT: $i\r"
   fi
 done
 
 echo ""
 echo ""
-echo "FortiWeb API ML trained with POST & GET methods on ${PETSTORE_URL}/"
+echo "FortiWeb API ML trained with POST/GET/PUT methods on ${PETSTORE_URL}/"
