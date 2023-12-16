2ed626ed8efdbed563e23e15927df063ff13ce72
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-12-15 23:19:42 -0500

    ml reset

---
+++ fl-demo/ml-reset.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Encode username and password in base64 and assign to TOKEN
+TOKEN=$(echo '{"username":"userapi","password":"userAPI123!"}' | base64)
+FWB_MGT_IP="10.163.7.21"
+
+# Pause and prompt for user to continue
+read -p "Press enter to continue..."
+
+echo ""
+echo "Getting ML Domain Info:"
+echo ""
+
+# Retrieve ML domain information
+result=$(curl --silent --insecure --location -g --request GET "https://${FWB_MGT_IP}/api/v2.0/machine_learning/policy.getdomaininfo?policy_name=${POLICY}" \
+--header "Authorization: ${TOKEN}" \
+--header 'accept: application/json' | jq)
+
+echo "$result"
+echo ""
+
+# Pause and prompt for user to continue
+read -p "Press enter to continue..."
+
+# Extract db_id and domain_name
+db_id=$(echo "$result" | jq '.results[0].db_id')
+domain_name=$(echo "$result" | jq '.results[0].domain_name')
+
+echo "Domain ${domain_name} has db_id ${db_id}"
+echo ""
+
+# Pause and prompt for user to continue
+read -p "Press enter to continue..."
+
+echo "Resetting Machine Learning for Domain ${domain_name}"
+echo ""
+
+# Execute the command with the extracted db_id value
+curl --insecure --location -g --request POST "https://${FWB_MGT_IP}/api/v2.0/machine_learning/policy.refreshdomain" \
+--header "Authorization: ${TOKEN}" \
+--header 'accept: application/json' \
+--data-raw '{"domain_id": "'${db_id}'", "policy_name": "'${POLICY}'"}'
+
+echo ""
+echo "Machine Learning for domain ${domain_name} has been reset. Press enter to continue..."
+read response
+
+return 1
