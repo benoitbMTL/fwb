b587a25cd162d8c6a7a810ab78d2913353ebfc1e
benoitbMTL <benoit@buonassera.fr>
2023-12-19 22:48:38 -0500

    new menu

--- fl-demo/petstore-demo.sh
+++ fl-demo/petstore-demo.sh
@@ -45,26 +45,187 @@ options=(
     'GET accept: application/yaml                         - Non JSON request'
 )
 
-function execute_curl() {
-    local command_index=$1
-    case $command_index in
-        0) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=available" -H "accept: application/json" -H "content-type: application/json" ;;
-        1) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        2) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=pending" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        3) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        4) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=ABCDEFGHIJKL" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        5) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=A" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        6) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=;cmd.exe" -H "Accept: application/json" -H "Content-Type: application/json" ;;
-        7) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 110, "name": "FortiCamel"}], "status": "/bin/ls"}' ;;
-        8) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 111, "name": "FortiCamel"}], "status": "xx& var1=l var2=s;$var1$var2"}' ;;
-        9) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": "<script>alert(123)</script>"}' ;;
-        10) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -F "file=@wso.php;type=application/x-php" -F "data={"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 113, "name": "FortiCamel"}], "status": "sold"};type=application/json" ;;
-        11) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -F "file=@eicar.com.txt;type=text/plain" -F "data={"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 113, "name": "FortiCamel"}], "status": "sold"};type=application/json" ;;
-        12) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": $WSO_CONTENT}' ;;
-        13) curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": "${EICAR_CONTENT}"}' ;;
-        14) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold&status=pending" -H "accept: application/json" -H "Content-Type: application/json" ;;
-        15) curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold" -H "accept: application/yaml" -H "Content-Type: application/json" ;;
-    esac
+function get_status_available() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=available"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_sold() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=sold"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_pending() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=pending"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_empty() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_long() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=ABCDEFGHIJKL"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_short() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=A"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_command_injection() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=;cmd.exe"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_status_command_injection() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='{"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 110, "name": "FortiCamel"}], "status": "/bin/ls"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_status_zero_day() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 111, "name": "FortiCamel"}], "status": "xx& var1=l var2=s;$var1$var2"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_status_xss() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": "<script>alert(123)</script>"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_upload_wso() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='-F "file=@wso.php;type=application/x-php" -F "data={"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 113, "name": "FortiCamel"}], "status": "sold"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_upload_eicar() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='-F "file=@eicar.com.txt;type=text/plain" -F "data={"id": 110, "category": {"id": 110, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 113, "name": "FortiCamel"}], "status": "sold"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_status_wso() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": $WSO_CONTENT}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function post_status_eicar() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="POST"
+    local URL="${PETSTORE_URL}/pet"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local DATA='{"id": 111, "category": {"id": 111, "name": "Camel"}, "name": "FortiCamel", "photoUrls": ["photo.png"], "tags": [ {"id": 112, "name": "FortiCamel"}], "status": "${EICAR_CONTENT}"}'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER $DATA"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_duplicate() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=sold&status=pending"
+    local HEADER='-H "Accept: application/json" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
+}
+
+function get_status_yaml() {
+    local COMMAND="curl -s -k -X"
+    local METHOD="GET"
+    local URL="${PETSTORE_URL}/pet/findByStatus?status=sold"
+    local HEADER='-H "Accept: application/yaml" -H "Content-Type: application/json"'
+    local FULL_COMMAND="$COMMAND $METHOD $URL $HEADER"
+
+    echo -e "${CYAN}Executing: $FULL_COMMAND${NC}"
+    eval "$FULL_COMMAND"
 }
 
 function select_option() {
@@ -85,10 +246,25 @@ function select_option() {
         return
     fi
 
-    clear
-    echo -e "${CYAN}Executing CURL command:${NC}"
-    local command_index=$((choice-1))
-    echo -e "${CYAN}${curl_commands[$command_index]}${NC}"
+    case $((choice-1)) in
+        0) get_status_available ;;
+        1) get_status_sold ;;
+        2) get_status_pending ;;
+        3) get_status_empty ;;
+        4) get_status_long ;;
+        5) get_status_short ;;
+        6) get_status_command_injection ;;
+        7) post_status_command_injection ;;
+        8) post_status_zero_day ;;
+        9) post_status_xss ;;
+        10) post_upload_wso ;;
+        11) post_upload_eicar ;;
+        12) post_status_wso ;;
+        13) post_status_eicar ;;
+        14) get_status_duplicate ;;
+        15) get_status_short ;;
+    esac
+
     echo
     local output=$(eval ${curl_commands[$command_index]} 2>&1)
     if [[ "$output" == "Request Blocked by FortiWeb!" ]]; then
