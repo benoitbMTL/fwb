babf303998f5188c6a66d5cae342160988e11833
benoitbMTL <benoit@buonassera.fr>
2023-12-19 23:06:47 -0500

    inverse quote

--- fl-demo/petstore-demo.sh
+++ fl-demo/petstore-demo.sh
@@ -29,27 +29,27 @@ options=(
     'POST {"status": "/bin/ls"}                            - Command Injection in JSON'
     'POST {"status": "xx& var1=l var2=s;\$var1\$var2"}     - Zero-Day in JSON'
     'POST {"status": "<script>alert(123)</script>"}        - XSS in JSON'
-    'POST file=@eicar.com.txt                              - Upload Eicar test file'
-    'POST {"status": "*eicar.com.txt*"}                    - Eicar in JSON'
-    'GET findByStatus?status=sold&status=pending           - Additional URL parameter'
-    'GET accept: application/yaml                          - Non JSON request'
+    'POST file=@eicar.com.txt                             - Upload Eicar test file'
+    'POST {"status": "*eicar.com.txt*"}                   - Eicar in JSON'
+    'GET findByStatus?status=sold&status=pending          - Additional URL parameter'
+    'GET accept: application/yaml                         - Non JSON request'
 )
 
 curl_commands=(
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=available" -H "accept: application/json" -H "content-type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=pending" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=ABCDEFGHIJKL" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=A" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=;cmd.exe" -H "Accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"/bin/ls\"}"'
-    'curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}"'
-    'curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 112, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}"'
-    'curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -F "file=@eicar.com.txt;type=text/plain" -F "data={\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 113, \"name\": \"FortiCamel\"}], \"status\": \"sold\"};type=application/json"'
-    'curl -s -k -X POST "${PETSTORE_URL}/pet" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 112, \"name\": \"FortiCamel\"}], \"status\": \"${EICAR_CONTENT}\"}"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold&status=pending" -H "accept: application/json" -H "Content-Type: application/json"'
-    'curl -s -k -X GET  "${PETSTORE_URL}/pet/findByStatus?status=sold" -H "accept: application/yaml" -H "Content-Type: application/json"'
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=available' -H 'accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=sold' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=pending' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=ABCDEFGHIJKL' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=A' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=;cmd.exe' -H 'Accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X POST '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json" -d '{\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 110, \"name\": \"FortiCamel\"}], \"status\": \"/bin/ls\"}'"
+    "curl -s -k -X POST '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json" -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 111, \"name\": \"FortiCamel\"}], \"status\": \"xx& var1=l var2=s;$var1$var2\"}'"
+    "curl -s -k -X POST '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json" -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 112, \"name\": \"FortiCamel\"}], \"status\": \"<script>alert(123)</script>\"}'"
+    "curl -s -k -X POST '${PETSTORE_URL}/pet' -H 'accept: application/json" -F "file=@eicar.com.txt;type=text/plain" -F "data={\"id\": 110, \"category\": {\"id\": 110, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 113, \"name\": \"FortiCamel\"}], \"status\": \"sold\"};type=application/json"'
+    "curl -s -k -X POST '${PETSTORE_URL}/pet' -H 'accept: application/json' -H 'Content-Type: application/json" -d '{\"id\": 111, \"category\": {\"id\": 111, \"name\": \"Camel\"}, \"name\": \"FortiCamel\", \"photoUrls\": [\"photo.png\"], \"tags\": [ {\"id\": 112, \"name\": \"FortiCamel\"}], \"status\": \"${EICAR_CONTENT}\"}'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=sold&status=pending' -H 'accept: application/json' -H 'content-type: application/json'"
+    "curl -s -k -X GET  '${PETSTORE_URL}/pet/findByStatus?status=sold' -H 'accept: application/yaml' -H 'content-type: application/json'"
 )
 
 select_option() {
